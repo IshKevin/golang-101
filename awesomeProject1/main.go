00242c786ed781d7bf2c05112efd0ddb09bfe2bc
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-10-20 21:33:54 +0200

    variable

--- awesomeProject1/main.go
+++ awesomeProject1/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello Go World")
+    var task1 = "Task 1 is to create a todo app" 
+	var emptyVar string
+	newTask := "Task 2 is to create a todo app"
+	fmt.Println("##### welcome to our Todo App #####")
+	fmt.Println(1)
+	fmt.Println(task1)
+	fmt.Println(emptyVar)
+	fmt.Println(newTask)
+	fmt.Println(task2)
+
 }
