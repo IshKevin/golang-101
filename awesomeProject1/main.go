99ddc738c7d5535ae82fdd754ccaafb275792247
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-10-20 21:44:24 +0200

    array and slice

--- awesomeProject1/main.go
+++ awesomeProject1/main.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-    var task1 = "Task 1 is to create a todo app" 
-	var emptyVar string
-	newTask := "Task 2 is to create a todo app"
+    // var task1 = "Task 1 is to create a todo app" 
+	// var emptyVar string
+	//newTask := "Task 2 is to create a todo app"
 	fmt.Println("##### welcome to our Todo App #####")
-	fmt.Println(1)
-	fmt.Println(task1)
-	fmt.Println(emptyVar)
-	fmt.Println(newTask)
-	fmt.Println(task2)
+	// fmt.Println(1)
+	// fmt.Println(task1)
+	// fmt.Println(emptyVar)
+	// fmt.Println(newTask)
+	//fmt.Println(task2)
+
+	arr := []string{"Task 1", "Task 2", "Task 3"}
+	fmt.Println("tasks",arr)
+	fmt.Println(arr[0])
+
 
 }
